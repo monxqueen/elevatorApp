059668a9851c689491b6fbc4fc69d131e81020a6
Glória Monique <gloriamonique123@outlook.com>
2021-05-30 21:35:23 -0300

    bugs fixed

--- app/src/main/java/com/monique/elevator_app/Elevator.kt
+++ app/src/main/java/com/monique/elevator_app/Elevator.kt
@@ -1,15 +1,18 @@
 package com.monique.elevator_app
 
-class Elevator(private var pplQuantity: Int = 0,
-               private var currentFloor: Int = 0,
-               private val limitPeople: Int = 5){
+class Elevator(private var pplQuantity: Int,
+               private var currentFloor: Int,
+               private val limitPeople: Int,
+               private val limitFloors: Int){
 
-    fun setQuantityToAdd(quant: Int): Int = (pplQuantity+quant)
-    fun setFloor(floor: Int): Int {
-        currentFloor = floor
-        return currentFloor
+    fun setPplQuantity(quant: Int){
+        pplQuantity += quant
     }
-    fun getQuant(): Int = pplQuantity
+    fun setFloor(floor: Int){
+        currentFloor = floor
+    }
+    fun getQuantPpl(): Int = pplQuantity
+    fun getQuantFloors(): Int = limitFloors
     fun getFloor(): Int = currentFloor
     fun getLimitPeople(): Int = limitPeople
 }
--- app/src/main/java/com/monique/elevator_app/MainActivity.kt
+++ app/src/main/java/com/monique/elevator_app/MainActivity.kt
@@ -3,86 +3,75 @@ package com.monique.elevator_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.google.android.material.textfield.TextInputLayout
 
-abstract class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var andarTxt: TextView
     private lateinit var qtdPessoasTxt: TextView
-    private lateinit var andarEdt: EditText
+    private lateinit var andarEdt: TextInputLayout
     private lateinit var submitBtn: Button
     private lateinit var entrarBtn: Button
     private lateinit var sairBtn: Button
-    private val qtdAndares = 7
-    private var limitePessoas = 5
-    private var qtdPessoas = 0
-
+    private val elevator = Elevator(0, 0, 5, 7)
+    private val qtdAndares = elevator.getQuantFloors()
+    private val limitePessoas = elevator.getLimitPeople()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //bindViews()
-        val elevator = Elevator()
+        bindViews()
+    }
+
+    private fun bindViews(){
         andarTxt = findViewById(R.id.floor)
         qtdPessoasTxt = findViewById(R.id.people)
         andarEdt = findViewById(R.id.floorInput)
         submitBtn = findViewById(R.id.submit)
         entrarBtn = findViewById(R.id.entry)
         sairBtn = findViewById(R.id.getoff)
-        /* qtdPessoas = elevator.getQuant()
-         limitePessoas = elevator.getLimitPeople()*/
 
-        submitBtn.setOnClickListener {
-            val andarInput = andarEdt.text.toString().toInt()
+        submitBtn.setOnClickListener{
+            val andarInput = andarEdt.editText?.text.toString().toInt()
+            println(andarInput)
 
-            if (andarInput > qtdAndares || andarInput < 0) {
-                Toast.makeText(
-                    this,
+            if(andarInput > qtdAndares || andarInput < 0){
+                Toast.makeText(this,
                     "Andar inválido, tente novamente",
-                    Toast.LENGTH_LONG
-                ).show()
-            } else if (andarInput == 0) {
+                    Toast.LENGTH_LONG).show()
+            }else if(andarInput == 0){
                 andarTxt.text = "térreo"
                 elevator.setFloor(0)
-                //elevator.currentFloor = 0
-            } else {
-                andarTxt.text = "Andar = ${elevator.getFloor()}"
-                //andarTxt.text = "${elevator.currentFloor}º \nandar"
+            }else{
                 elevator.setFloor(andarInput)
+                andarTxt.text = "${elevator.getFloor()}º \nandar"
             }
         }
 
-        entrarBtn.setOnClickListener {
+        entrarBtn.setOnClickListener{
 
-            if (qtdPessoas == limitePessoas) {
-                Toast.makeText(
-                    this,
+            if(elevator.getQuantPpl() == limitePessoas){
+                Toast.makeText(this,
                     "Elevador com capacidade máxima, espere alguém sair",
-                    Toast.LENGTH_LONG
-                ).show()
-            } else {
-                elevator.setQuantityToAdd(1)
-                //elevator.pplQuantity += 1
-                qtdPessoas += 1
+                    Toast.LENGTH_LONG).show()
+            }else{
+                elevator.setPplQuantity(1)
             }
-            qtdPessoasTxt.text = "Qtd de pessoas = $qtdPessoas"
+            qtdPessoasTxt.text = "${elevator.getQuantPpl()}/$limitePessoas"
         }
 
-        sairBtn.setOnClickListener {
-            if (qtdPessoas == 0) {
-                Toast.makeText(
-                    this,
+        sairBtn.setOnClickListener{
+            if(elevator.getQuantPpl() == 0){
+                Toast.makeText(this,
                     "Ninguém no elevador para sair",
-                    Toast.LENGTH_LONG
-                ).show()
-            } else {
-                elevator.setQuantityToAdd(-1)
-                //elevator.pplQuantity -= 1
-                qtdPessoas -= 1
+                    Toast.LENGTH_LONG).show()
+            }else{
+                elevator.setPplQuantity(-1)
             }
-            qtdPessoasTxt.text = "Qtd de pessoas = $qtdPessoas"
+            qtdPessoasTxt.text = "${elevator.getQuantPpl()}/$limitePessoas"
         }
+
     }
 }
